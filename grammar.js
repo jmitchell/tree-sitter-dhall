f8041318cec7deaa08b98c1232fa728530d967b8
Jacob Mitchell <jmitchell@member.fsf.org>
2019-02-27 13:13:21 -0800

    Remove incorrect comment about hex digit case sensitivity.
    @Gabriel439 reminded me in
    https://github.com/dhall-lang/dhall-lang/issues/389 that strings in
    ABNF (RFC 5234) grammars are case-insensitive, so the HEXDIG "A"
    matches "a" as well.

--- grammar.js
+++ grammar.js
@@ -190,10 +190,6 @@ module.exports = grammar({
     DIGIT: $ =>
       choice("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"),
     HEXDIG: $ =>
-      // dhall.abnf doesn't support lower-case hex, but they're
-      // commonly used.
-      //
-      // See https://github.com/dhall-lang/dhall-lang/issues/389
       choice($.DIGIT, "A", "B", "C", "D", "E", "F", "a", "b", "c", "d", "e", "f"),
     // ; A simple label cannot be one of the following reserved names:,
     // ;,
